d9ed99d24344bbcb70f686d024e422473f0ed273
marlonrmv85 <123046046@upq.edu.mx>
2025-01-30 21:45:00 -0600

    Ejercicio_2

--- Practica_5/src/Ejercicio_2.java
+++ Practica_5/src/Ejercicio_2.java
@@ -7,5 +7,22 @@ public class Ejercicio_2 {
 
 //        2. Escribir un programa que pida al usuario un número entero positivo y muestre
 //        por pantalla la cuenta atrás desde ese número hasta cero separados por comas.
+
+        System.out.println("ingresa un numero entero positivo");
+        int numero = sc.nextInt();
+
+        if (numero > 0) {
+            System.out.print("Cuenta atrás desde " + numero + " hasta 0: ");
+            for (int i = numero; i >= 0; i--) {
+                System.out.print(i);
+                if (i > 0) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        } else {
+            System.out.println("El número debe ser un entero positivo.");
+        }
+
     }
 }
