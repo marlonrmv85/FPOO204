ac7bf99babe36dd7e73b2b253d7968051cbc6dd1
marlonrmv85 <123046046@upq.edu.mx>
2025-09-03 17:11:14 -0600

    Tructru
    hice tal y tal

---
+++ Final_parcial2/final/src/pkgfinal/Conversor.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author marlo
+ */
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Conversor {
+    
+    private int numero;
+
+    Conversor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    {
+        
+    
+    JOptionPane.showMessageDialog(null, "ingresa el numero que quieres convertir");
+    }
+    
+    {
+  
+        
+    
+      //if( numero == null) {
+            JOptionPane.showMessageDialog(null, "Faltaron datos, intenta de nuevo.");
+            
+        }
+
+    }
+    
+    
+    
+   
+        
+
+    
+
+
+   
---
+++ Final_parcial2/final/src/pkgfinal/Final.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author marlo
+ */
+
+import java.util.*;
+import javax.swing.JOptionPane;
+public class Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+     
+  
+         Conversor con = new Conversor();{
+        con.ejecutar();
+    }
+         
+         
+    }
+    
+    
+}
